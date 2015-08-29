8acd499424f1664ea9af9b6c78985ac552953a3a
Jiming Ye <415386365@qq.com>
2015-08-29 22:28:01 +0800

    Update process_csv.py

--- src/process_csv.py
+++ src/process_csv.py
@@ -1,7 +1,7 @@
 import csv
 import sys
 import cPickle as pickle
-csv.field_size_limit(sys.maxsize)
+csv.field_size_limit(999999999)
 
 import numpy as np
 from scipy.sparse import csr_matrix, lil_matrix
@@ -12,58 +12,65 @@ PATH_TO_TRAIN = "datasets/trainTags.csv"
 PATH_TO_TEST = "datasets/testTags.csv"
 
 
-def get_train_feature(path_to_train, col):
-    traincsv = csv.reader(open(path_to_train))
-    feature = []
-    for t, i in enumerate(traincsv):
-        if t == 0:
-            continue
-        feature.append(i[col])
+def get_feature(path, col):
+    """ Get feature values for given column index
+    parameters:
+    --------------------------------------------------------
+    path: path to the file
+    col: column index
+    """
+    csvfile = csv.reader(open(path))
+    feature = [i[col] for t, i in enumerate(csvfile) if t!= 0]
     return feature
 
 
-def get_test_matrix(path_to_test, tfv, col, n):
-    """ Transform one column of test data into sparse matrix
+def get_train_matrix(path):
+    """ Transform training data into sparse matrices
     parameters:
     --------------------------------------------------------
-    path_to_test: path to the test file
-    tfv: the trained tfidf vectorizer
-    col: the column to be transformed
-    n: width of the output matrix
+    path: path to the training file
     """
-    testcsv = csv.reader(open(path_to_test))
-    idx = [i[0] for t, i in enumerate(testcsv) if t!= 0]
-    m = len(idx)
-    testcsv = csv.reader(open(path_to_test))
-    sparse_matrix = lil_matrix((m, n))
+    idx = get_feature(path, 0)
+    tags = get_feature(path, 1)
+    attrs = get_feature(path, 2)
+    y = get_feature(path, 3)
+    
+    tfv_tag = TfidfVectorizer(min_df=1, max_features=None, strip_accents='unicode',
+                              analyzer='word', token_pattern=r'\w{1,}',
+                              ngram_range=(1, 3), use_idf=1, smooth_idf=1,
+                              sublinear_tf=1, stop_words=None)
+    tfv_attr = TfidfVectorizer(min_df=1, max_features=None, strip_accents='unicode',
+                               analyzer='word', token_pattern=r'\w{1,}',
+                               ngram_range=(1, 1), use_idf=1, smooth_idf=1,
+                               sublinear_tf=1, stop_words=None)
+    tags = tfv_tag.fit_transform(tags)
+    attrs = tfv_attr.fit_transform(attrs)
 
-    for t, line in enumerate(testcsv):
-        if t == 0:
-            continue
-        sparse_matrix[t-1] = tfv.transform([line[col]])
-
-    return idx, csr_matrix(sparse_matrix)
+    return idx, tags, attrs, y, tfv_tag, tfv_attr
 
 
-print "get tags matrix for train"
-train_idx = get_train_feature(PATH_TO_TRAIN, 0)
-y = get_train_feature(PATH_TO_TRAIN, 3)
-train_tags = get_train_feature(PATH_TO_TRAIN, 1)
-tfv_tag = TfidfVectorizer(min_df=1, max_features=None, strip_accents='unicode',
-                          analyzer='word', token_pattern=r'\w{1,}',
-                          ngram_range=(1, 2), use_idf=1, smooth_idf=1,
-                          sublinear_tf=1, stop_words=None)
-train_tags = tfv_tag.fit_transform(train_tags)
+def get_test_matrix(path, tfv_tag, tfv_attr):
+    """ Transform test data into sparse matrices
+    parameters:
+    --------------------------------------------------------
+    path: path to the test file
+    tfv_tag: the trained tfidf vectorizer for tag
+    tfv_attr: the trained tfidf vectorizer for attribute
+    """
+    idx = get_feature(path, 0)
+    tags = get_feature(path, 1)
+    attrs = get_feature(path, 2)
 
-print "get attributes matrix for train"
-train_attrs = get_train_feature(PATH_TO_TRAIN, 2)
-tfv_attr = TfidfVectorizer(min_df=1, max_features=None, strip_accents='unicode',
-                           analyzer='word', token_pattern=r'\w{1,}',
-                           ngram_range=(1, 1), use_idf=1, smooth_idf=1,
-                           sublinear_tf=1, stop_words=None)
-train_attrs = tfv_attr.fit_transform(train_attrs)
+    tags = tfv_tag.transform(tags)
+    attrs = tfv_attr.transform(attrs)
 
-print "save the matrices"
+    return idx, tags, attrs
+
+
+print "get train matrices"
+train_idx, train_tags, train_attrs, y, tfv_tag, tfv_attr = get_train_matrix(PATH_TO_TRAIN)
+           
+print "save train matrices"
 with open("datasets/y.pkl", "w") as f:
     pickle.dump(y, f)
     f.close()
@@ -78,24 +85,18 @@ with open("datasets/trainAttrSparse.pkl", "w") as f:
     f.close()
 
 # collect garbage
-m, n1 = train_tags.shape
-m, n2 = train_attrs.shape
 train_tags = train_attrs = None
 
-print "get tags matrix for test"
-test_idx, test_tags = get_test_matrix(PATH_TO_TEST, tfv_tag, 1, n1)
+print "get test matrices"
+test_idx, test_tags, test_attrs = get_test_matrix(PATH_TO_TEST, tfv_tag, tfv_attr)
+
+print "save test matrices"
 with open("datasets/test_idx.pkl", "w") as f:
     pickle.dump(test_idx, f)
     f.close()
 with open("datasets/testTagSparse.pkl", "w") as f:
     pickle.dump(test_tags, f)
     f.close()
-
-# collect garbage
-test_tags = None
-
-print "get atrributes matrix for test"
-test_idx, test_attrs = get_test_matrix(PATH_TO_TEST, tfv_attr, 2, n2)
 with open("datasets/testAttrSparse.pkl", "w") as f:
     pickle.dump(test_attrs, f)
     f.close()
