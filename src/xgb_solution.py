12e0587ec538d6f95b93f8e08fa3d2ed8b4ad08c
Jiming Ye <415386365@qq.com>
2015-08-29 22:42:08 +0800

    Update xgb_solution.py

--- src/xgb_solution.py
+++ src/xgb_solution.py
@@ -6,7 +6,6 @@ import numpy as np
 
 from scipy.sparse import csr_matrix, hstack
 from sklearn.decomposition import TruncatedSVD
-from sklearn.preprocessing import MinMaxScaler
 
 
 with open("datasets/trainTagSparse.pkl") as f:
@@ -15,11 +14,11 @@ with open("datasets/trainTagSparse.pkl") as f:
 with open("datasets/trainAttrSparse.pkl") as f:
     X_attr = pickle.load(f)
 
-##with open("datasets/testTagSparse.pkl") as f:
-##    X_test_tag = pickle.load(f)
-##
-##with open("datasets/testAttrSparse.pkl") as f:
-##    X_test_attr = pickle.load(f)
+with open("datasets/testTagSparse.pkl") as f:
+    X_test_tag = pickle.load(f)
+
+with open("datasets/testAttrSparse.pkl") as f:
+    X_test_attr = pickle.load(f)
 
 with open("datasets/y.pkl") as f:
     label = pickle.load(f)
@@ -40,18 +39,15 @@ print "Using Xgboost..."
 dtrain = xgb.DMatrix(X, label=y)
 param = {"objective":"binary:logistic", "nthread":8,
          "eval_metric":"auc", "bst:max_depth":30, 
-         "bst:min_child_weight":1, "bst:subsample":1.0,
-         "bst:colsample_bytree":1.0, "bst:eta":0.05}
+         "bst:min_child_weight":1, "bst:subsample":0.7,
+         "bst:colsample_bytree":0.7, "bst:eta":0.05}
 num_round = 300
 
 print "Training..."
-bst = xgb.cv(param, dtrain, num_round, nfold=5)
-##bst = xgb.train(param, dtrain, num_round)
-##
-##print "Predicting..."
-##dtest = xgb.DMatrix(X_test)
-##prediction = bst.predict(dtest)
-##scalar = MinMaxScaler()
-##prediction = scalar.fit_transform(prediction)
-##submission = pd.DataFrame({"id": test_idx, "prediction": prediction})
-##submission.to_csv("submissions/xgboost.csv", index=False)
+bst = xgb.train(param, dtrain, num_round)
+
+print "Predicting..."
+dtest = xgb.DMatrix(X_test)
+prediction = bst.predict(dtest)
+submission = pd.DataFrame({"id": test_idx, "prediction": prediction})
+submission.to_csv("submissions/xgboost.csv", index=False)
