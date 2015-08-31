828979f01c4a9d527c28e8f0a52133536a852136
Jiming Ye <415386365@qq.com>
2015-08-31 22:09:21 +0800

    Update and rename xgb_solution.py to truly_native_model.py

---
+++ src/truly_native_model.py
@@ -0,0 +1,82 @@
+import xgboost as xgb
+import cPickle as pickle
+
+import pandas as pd
+import numpy as np
+
+from scipy.sparse import csr_matrix, hstack
+from sklearn.decomposition import TruncatedSVD
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+
+
+with open("datasets/trainTagSparse.pkl") as f:
+    X_tag = pickle.load(f)
+
+with open("datasets/trainAttrSparse.pkl") as f:
+    X_attr = pickle.load(f)
+
+with open("datasets/testTagSparse.pkl") as f:
+    X_test_tag = pickle.load(f)
+
+with open("datasets/testAttrSparse.pkl") as f:
+    X_test_attr = pickle.load(f)
+
+with open("datasets/y.pkl") as f:
+    label = pickle.load(f)
+    y = np.array([int(i) for i in label])
+
+with open("datasets/train_idx.pkl") as f:
+    train_idx = pickle.load(f)
+
+with open("datasets/test_idx.pkl") as f:
+    test_idx = pickle.load(f)
+
+
+def training(model):
+    X = csr_matrix(hstack((X_tag, X_attr)))
+    X_test = csr_matrix(hstack((X_test_tag, X_test_attr)))
+
+    if model != "lr":
+        print "Performing SVD..."
+        svd = TruncatedSVD(n_components=200, n_iter=5)
+        X = svd.fit_transform(X)
+        X_test = svd.transform(X_test)
+
+    if model == "xgb":
+        print "Using Xgboost..."
+        dtrain = xgb.DMatrix(X, label=y)
+        param = {"objective":"binary:logistic", "nthread":8,
+                 "eval_metric":"auc", "bst:max_depth":30, 
+                 "bst:min_child_weight":1, "bst:subsample":0.7,
+                 "bst:colsample_bytree":0.7, "bst:eta":0.01}
+        num_round = 1200
+        print "Training..."
+        bst = xgb.train(param, dtrain, num_round)
+        print "Predicting..."
+        dtest = xgb.DMatrix(X_test)
+        prediction = bst.predict(dtest)
+
+    elif model == "lr":
+        print "Using Logistic Regression..."
+        clf = LogisticRegression(C=3.0, random_state=1234)
+        clf.fit(X, y)
+        prediction = clf.predict_proba(X_test)
+        prediction = prediction[:, 1]
+
+    elif model == "rf":
+        forest = RandomForestClassifier(n_estimators=500, random_state=1234, max_features=100, n_jobs=4)
+        forest.fit(X, y)
+        prediction = forest.predict_proba(X_test)
+        prediction = prediction[:, 1]
+
+    else:
+        raise ValueError("model must be Random Forest(rf), GBDT(xgb) or Logistic Regression(lr)")
+
+    return prediction
+
+
+if __name__ == "__main__":
+    prediction = training(model="lr")
+    submission = pd.DataFrame({"id": test_idx, "prediction": prediction})
+    submission.to_csv("submissions/logistic.csv", index=False)
