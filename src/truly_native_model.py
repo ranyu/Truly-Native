2a58541bd6e61c6ec660600b95501416ac4ceff2
Jiming Ye <415386365@qq.com>
2015-09-06 21:52:59 +0800

    Update truly_native_model.py

--- src/truly_native_model.py
+++ src/truly_native_model.py
@@ -10,32 +10,35 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 
 
-with open("datasets/trainTagSparse.pkl") as f:
-    X_tag = pickle.load(f)
-
-with open("datasets/trainAttrSparse.pkl") as f:
-    X_attr = pickle.load(f)
-
-with open("datasets/testTagSparse.pkl") as f:
-    X_test_tag = pickle.load(f)
-
-with open("datasets/testAttrSparse.pkl") as f:
-    X_test_attr = pickle.load(f)
-
+with open("datasets/train_idx.pkl") as f:
+    train_idx = pickle.load(f)
+with open("datasets/test_idx.pkl") as f:
+    test_idx = pickle.load(f)
 with open("datasets/y.pkl") as f:
     label = pickle.load(f)
     y = np.array([int(i) for i in label])
 
-with open("datasets/train_idx.pkl") as f:
-    train_idx = pickle.load(f)
-
-with open("datasets/test_idx.pkl") as f:
-    test_idx = pickle.load(f)
-
 
 def training(model):
-    X = csr_matrix(hstack((X_tag, X_attr)))
-    X_test = csr_matrix(hstack((X_test_tag, X_test_attr)))
+    print "reading training data..."
+    with open("datasets/trainTagSparse.pkl") as f:
+        X_tag = pickle.load(f)
+    with open("datasets/trainAttrSparse.pkl") as f:
+        X_attr = pickle.load(f)
+    with open("datasets/trainValueSparse.pkl") as f:
+        X_value = pickle.load(f)
+    X = csr_matrix(hstack((X_tag, X_attr, X_value)))
+    X_tag = X_attr = X_value = None
+
+    print "reading test data..."
+    with open("datasets/testTagSparse.pkl") as f:
+        X_test_tag = pickle.load(f)
+    with open("datasets/testAttrSparse.pkl") as f:
+        X_test_attr = pickle.load(f)
+    with open("datasets/testValueSparse.pkl") as f:
+        X_test_value = pickle.load(f)
+    X_test = csr_matrix(hstack((X_test_tag, X_test_attr, X_test_value)))
+    X_test_tag = X_test_attr = X_test_value = None
 
     if model != "lr":
         print "Performing SVD..."
@@ -65,6 +68,7 @@ def training(model):
         prediction = prediction[:, 1]
 
     elif model == "rf":
+        print "Using Random Forest..."
         forest = RandomForestClassifier(n_estimators=500, random_state=1234, max_features=100, n_jobs=4)
         forest.fit(X, y)
         prediction = forest.predict_proba(X_test)
@@ -77,6 +81,6 @@ def training(model):
 
 
 if __name__ == "__main__":
-    prediction = training(model="lr")
+    prediction = training(model="xgb")
     submission = pd.DataFrame({"id": test_idx, "prediction": prediction})
-    submission.to_csv("submissions/logistic.csv", index=False)
+    submission.to_csv("submissions/xgboost.csv", index=False)
